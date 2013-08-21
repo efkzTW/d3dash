90f8162085d36c63111588e1dfbfbacbdff649e4
Peter Wu <peian.peter.wu@gmail.com>
2013-08-21 20:19:38 +0800

    used actual csv data and changedpage background to white

--- dash1/js/dash1.js
+++ dash1/js/dash1.js
@@ -1,31 +1,68 @@
-var dataset = [];
-for (var i = 0; i < 20; i++) {
-	var newNum = Math.floor(Math.random() * 100);
-	dataset.push(newNum);
-}
 
-var w = 450;
-var h = 200;
-var margin = 2;
-svg = d3.select("#stb-bar")
-		.append("svg")
-		.attr({
-			width: w,
-			height: h
-		});
+var margin = {top: 10, right: 20, bottom: 25, left: 50},
+	width = 500 - margin.left - margin.right,
+	height = 220 - margin.top - margin.bottom;
 
-var xScale = d3.scale.ordinal()
-			.domain(d3.range(dataset.length))
-			.rangeBands([0,w],0.2);
+var x = d3.time.scale()
+		.range([0,width]);
 
-svg.selectAll("rect")
-	.data(dataset)
-	.enter()
-	.append("rect")
-	.attr({
-		x: function(d,i) {return xScale(i)},
-		y: function(d) {return h - (h/100 * d)},
-		width: xScale.rangeBand(),
-		height: function(d) { return h/100*d},
-		"fill": "#000"
+var y = d3.scale.linear()
+		.range([height, 0]);
+
+var parseDate = d3.time.format("%m/%d/%Y").parse;
+
+var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom");
+
+var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left");
+
+var line = d3.svg.line()
+		.x(function(d){return x(d.date);})
+		.y(function(d){return y(d.total);});
+
+var svg = d3.select("#stb-line").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("stb_data.csv", function(error, data) {
+	data.forEach(function(d) {
+		d.date = parseDate(d.date);
+		d.total = +d.total;
 	});
+
+	x.domain(d3.extent(data, function(d) {return d.date;}));
+	y.domain(d3.extent(data, function(d) {return d.total;}));
+
+	svg.append("g")
+		  .attr("class", "x axis")
+		  .attr("transform", "translate(0," + height + ")")
+		  .call(xAxis);
+
+	svg.append("g")
+		  .attr("class", "y axis")
+		  .call(yAxis)
+		.append("text")
+		  .attr("transform", "rotate(-90)")
+		  .attr("y", 6)
+		  .attr("dy", ".71em")
+		  .style("text-anchor", "end")
+		  .text("(activations)");
+
+	svg.append("text")
+		.attr("transform", "translate(300,10)")
+		.text("High: ");
+
+	svg.append("text")
+		.attr("transform", "translate(300, 30)")
+		.text("Low: ");
+
+	svg.append("path")
+		  .datum(data)
+		  .attr("class", "line")
+		  .attr("d", line);
+});

d82d098b839371a04d4f8b62f98b98873f688710
Peter Wu <peian.peter.wu@gmail.com>
2013-09-01 23:34:42 +0800

    finished working bar chart

--- dash3/js/stb_bar.js
+++ dash3/js/stb_bar.js
@@ -1,36 +1,87 @@
-var fullWidth = 255, fullHeight= 150;
-var margin = {top: 5, right: 2, bottom: 5, left: 2},
-	width = fullWidth - margin.left - margin.right,
-	height = fullHeight - margin.top - margin.bottom;
+//rawDate = data, key1 = data key1, key2 = data key2, id = dom id,
+function buildBarChart(dataSrc,id,xAxisFormat,yAxisRange){
 
-var parseDate = d3.time.format("%Y-%m-%d").parse;
+	var fullWidth = 255, fullHeight= 100;
+	var margin = {top: 10, right: 10, bottom: 15, left: 10},
+		width = fullWidth - margin.left - margin.right,
+		height = fullHeight - margin.top - margin.bottom;
 
-d3.json("data/daily_report.json", function(error, json) {
-	if (error) return console.warn(error);
-	dataset = json;
-	//dataset.forEach(function(d) {
-		
-	for (var i=0;i < Object.keys(dataset).length;i++) {
-		var object = Object.keys(dataset)[i];
-		if (parseDate(object) == null) {
-			console.log(object + " is null");
-		} else {
-			console.log(object + " is date");
+	var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+	var xScale = d3.scale.ordinal()
+			.rangeRoundBands([0,width],.2);
+
+	var yScale = d3.scale.linear()
+			.range([height,0]);
+
+	var xAxis = d3.svg.axis()
+			.scale(xScale)
+			.orient("bottom")
+			.tickFormat(d3.time.format(xAxisFormat));
+
+	var canvas = d3.select(id).append("svg")
+				.attr("width", fullWidth)
+				.attr("height", fullHeight)
+			.append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.json(dataSrc.rawData, function(error, json) {
+		if (error) return console.warn(error);
+		var dataset = [];
+
+		for (perDate in json) {
+			if (parseDate(perDate)!==null){
+				dataset.push({date:perDate, val:json[perDate][dataSrc.key1][dataSrc.key2]});
+			}
 		}
-	}
-});
+		//convert string to respective datatype
+		dataset.forEach(function(d){
 
-/*
-var xScale = d3.scale.ordinal()
-		.domain(dataset.map(function(d){return d.date;}))
-		.range([0,width]);
+			d.val = +d.val;
+			d.date = parseDate(d.date);
+			return d;
+		});
+		// sort object array by date
+		dataset.sort(function(a,b){return a.date-b.date;}); 
+		console.log(dataset);
 
-var yScale = d3.scale.linear()
-		.domain()
-		.range();
-*/
-var canvas = d3.select("#tw-daily").append("svg")
-			.attr("width", fullWidth)
-			.attr("height", fullHeight)
-		.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		yScale.domain([0,d3.max(dataset, function(d){return d.val;})]);
+		xScale.domain(dataset.map(function(d){return d.date;}));
+
+		canvas.selectAll("rect")
+			.data(dataset)
+			.enter()
+			.append("rect")
+				.attr("x", function(d,i){return xScale(d.date);})
+				.attr("y", function(d){return height;})
+				.attr("width", xScale.rangeBand())
+				.attr("height", 0)
+				.transition()
+				.delay(500)
+				.duration(1000)
+				.attr("y", function(d){return yScale(d.val);})
+				.attr("height", function(d){return height-yScale(d.val);});
+		
+		canvas.selectAll("text")
+			.data(dataset)
+			.enter()
+			.append("text")
+				.attr("x", function(d,i){return xScale(d.date) + xScale.rangeBand()/2;})
+				.attr("y", height-2)
+				.attr("class", "text")
+				.style("text-anchor", "middle")
+				.text(0).transition()
+						.delay(500)
+						.duration(1000)
+				.text(function(d){return d.val;})
+				.attr("y", function(d){return (yScale(d.val)-2);})
+
+		canvas.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + (height-5) + ")")
+			.call(xAxis);
+	});
+}
+
+buildBarChart({rawData:"data/daily_report.json",key1:"4",key2:"TD"}, "#tw-daily","%a");
+buildBarChart({rawData:"data/daily_report.json",key1:"4",key2:"ND"}, "#ntb-daily","%a");
